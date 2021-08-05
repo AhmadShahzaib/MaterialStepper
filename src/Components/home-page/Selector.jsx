40a8f21c254656c29dae7dcf3eada7397d3e2d7e
unknown <ahamdshahzib683@gmail.com>
2021-08-05 13:27:40 +0500

    5/8

--- src/Components/home-page/Selector.jsx
+++ src/Components/home-page/Selector.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  NativeSelect,
+} from "@material-ui/core";
 function Selector() {
   const classes = useStyles();
   const [product, setProduct] = React.useState("");
@@ -9,7 +15,7 @@ function Selector() {
   };
   return (
     <>
-      <FormControl variant="filled" className={classes.formControl}>
+      {/* <FormControl variant="filled" className={classes.formControl}>
         <InputLabel id="Stock-label">Stock Search</InputLabel>
         <Select
           labelId="Stock-label"
@@ -25,6 +31,22 @@ function Selector() {
           <MenuItem value="TMS">TMS</MenuItem>
           <MenuItem value="PGR">PGR</MenuItem>
         </Select>
+      </FormControl> */}
+      <FormControl variant="outlined" className={classes.formControl}>
+        <select
+          className={classes.SelectButton}
+          value="Stock Search"
+          onChange={handleChange}
+          label="Stock Search"
+          //   inputProps={{
+          //     name: "age",
+          //     id: "outlined-age-native-simple",
+          //   }}
+        >
+          <option value="KCH">KCH</option>
+          <option value="TMS">TMS</option>
+          <option value="PGR">PGR</option>
+        </select>
       </FormControl>
     </>
   );
@@ -44,6 +66,8 @@ const useStyles = makeStyles((theme) => ({
   SelectButton: {
     backgroundColor: "#ECECEC",
     color: "#5A5A5A",
+    border: "none",
+    padding: "10px 17px",
     textTransform: "none",
     borderRadius: "22px",
     boxShadow: "none",
